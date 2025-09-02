3a31a709ca67dff15ec0a1f037f470e3f4c9369a
JonasMaldo <isc.jonas.maldonado@gmail.com>
2025-09-02 07:49:37 -0600

    grand total and delete button added to car table

--- src/app/components/car/car.ts
+++ src/app/components/car/car.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
@@ -9,4 +9,18 @@ import { CartItem } from '../../models/cartItem';
 })
 export class Car {
   @Input() items: CartItem[] = []; //inyect from parent
+
+  @Input() total = 0
+
+  @Output() idProductEventEmmiter = new EventEmitter();
+
+  /**
+   * Emits the ID of the product to be deleted from the cart.
+   *
+   * @param id - The unique identifier of the product to remove from the cart.
+   */
+  onDeleteCart(id: number) {
+    this.idProductEventEmmiter.emit(id);
+  }
+
 }
--- src/app/components/cart-app/cart-app.ts
+++ src/app/components/cart-app/cart-app.ts
@@ -11,28 +11,74 @@ import { Car } from '../car/car';
   templateUrl: './cart-app.html',
   styleUrl: './cart-app.css'
 })
-export class CartApp implements OnInit{
+export class CartApp implements OnInit {
 
   products: Product[] = [];
 
   items: CartItem[] = [];
 
+  total: number = 0;
+
   //inject dependency
-  constructor(private service: ShopCarService){
+  constructor(private service: ShopCarService) {
 
   }
 
   //interface from onInit to initialize with this data
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.calculateTotal();
   }
 
-  onAddCart(product: Product){
-    //by immutability we updated the array of items to don't share on memory reference with the old object and prevent changes from other places thats why we used ... in both this.items and product
-    this.items = [... this.items, 
-      {product: {...product}, 
-      quantity: 1}]
+  /**
+   * Adds a product to the cart. If the product already exists in the cart, increments its quantity by 1.
+   * Otherwise, adds the product to the cart with a quantity of 1.
+   *
+   * @param product - The product to add to the cart.
+   */
+  onAddCart(product: Product) {
+    const hasItem = this.items.find(item => item.product.id === product.id);
+    if (hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id === product.id) {
+          return { ...item, quantity: item.quantity + 1 }
+        }
+        return item;
+      })
+    }
+    else {
+      //by immutability we updated the array of items to don't share on memory reference with the old object and prevent changes from other places thats why we used ... in both this.items and product
+      this.items = [... this.items,
+      {
+        product: { ...product },
+        quantity: 1
+      }]
+    }
+    this.calculateTotal();
   }
 
+  /**
+   * Removes an item from the cart based on the provided product ID.
+   *
+   * @param id - The unique identifier of the product to be removed from the cart.
+   */
+  onDeleteCart(id: number) {
+    this.items = this.items.filter(item => item.product.id !== id);
+    this.calculateTotal();
+  }
 
+  /**
+   * Calculates the total cost of all items in the cart.
+   * 
+   * Iterates over the `items` array, multiplying each item's quantity by its product price,
+   * and sums the results to update the `total` property.
+   *
+   * @remarks
+   * Assumes that each item in `items` has a `quantity` and a `product` with a `price` property.
+   *
+   * @returns {void}
+   */
+  calculateTotal(){
+    this.total = this.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0);
+  }
 }
